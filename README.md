d89dc1621fce61f0c13e47ef335611717b603eaa
pdemeu <pdemeu.pessoal@gmail.com>
2024-07-16 04:27:13 -0300

    feat: add git in project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project frontend boiler plate
+
+<img src="./image.png" alt="welcome">
+
+```
+yarn
+yarn dev
+```
+
+``http://localhost:5173/``
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-950">
+      <h1 className="text-white text-center  text-5xl mb-10">Bem vindo</h1>
+      <p className="text-white text-2xl">ReactJS + TypeSript + TailwindCSS + Vitest</p>
+      <p className="text-white text-2xl">RecoilJS + React Router DOM + React Toastify</p>
+    </div>
+  );
+}
+
+export default App;
